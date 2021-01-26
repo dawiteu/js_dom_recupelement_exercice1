878ff2f4b90b5d1aaebcd8133ce9d4f2b362c904
DawiT <Dawid@Tararuj.be>
2021-01-26 09:30:48 +0100

    fin

---
+++ main.js
@@ -0,0 +1,22 @@
+const h1s = document.getElementsByTagName('h1'); 
+
+console.log(h1s); 
+
+
+
+const h3s = document.getElementsByTagName('h3'); 
+
+for(let i=0; i<h3s.length; i++){
+    console.log(h3s[i]); 
+}
+
+
+const ps = document.getElementsByTagName('p'); 
+
+const ps2 = ps[1]; 
+
+
+const lis = document.getElementsByTagName('li'); 
+const li3 = lis[2]; 
+
+console.log(li3); 
